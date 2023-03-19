d01cbfc4c03125000d5ae0695a6cf90d8e56488e
toocels <toocels@gmail.com>
2023-03-19 11:07:18 +0530

    hmm forgot to commit with every change today, anyway, done

--- public/main.js
+++ public/main.js
@@ -1,7 +1,31 @@
-console.log("sup")
+var prev_search = ""
+var all_books;
+
+window.setInterval(() => {
+	var input_field = document.getElementById("search_books").value
+	if (input_field != prev_search) {
+		document.getElementById("list_of_books").innerHTML = "" // clear all blogs;
+
+		renderTableHeading();
+		for (var i in all_books) {
+			if (all_books[i]["name"].toLowerCase().includes(input_field))
+				renderBookList(all_books[i]["id"], all_books[i]["name"], all_books[i]["author"], all_books[i]["description"]);
+			else if (all_books[i]["author"].toLowerCase().includes(input_field))
+				renderBookList(all_books[i]["id"], all_books[i]["name"], all_books[i]["author"], all_books[i]["description"]);
+			else if (all_books[i]["description"].toLowerCase().includes(input_field))
+				renderBookList(all_books[i]["id"], all_books[i]["name"], all_books[i]["author"], all_books[i]["description"]);
+		}
+		window.scrollTo(0, document.body.scrollHeight);
+
+		prev_search = input_field
+	}
+}, 200);
+
+refreshBookList();
 
 function req(type, urlParm, body_data = undefined) {
-	const url = window.location.origin + window.location.pathname + 'api/' + urlParm;
+	// const url = window.location.origin + window.location.pathname + 'api/' + urlParm;
+	const url = "http://localhost/api/" + urlParm
 	if (body_data != undefined) {
 		return fetch(url, {
 				method: type,
@@ -23,44 +47,60 @@ function req(type, urlParm, body_data = undefined) {
 	return null;
 }
 
-function renderBookList(id, name, author, desc) {
-	var list_x = document.createElement("div");
+function renderTableHeading() {
+	var table = document.getElementById("list_of_books");
 
-	book_id = document.createElement("span");
-	book_name = document.createElement("span");
-	book_author = document.createElement("span");
-	book_desc = document.createElement("span");
+	var row = table.insertRow(0);
 
-	book_id.innerHTML = id
-	book_name.innerHTML = name
-	book_author.innerHTML = author
-	book_desc.innerHTML = desc
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
 
-	list_x.appendChild(book_id)
-	list_x.appendChild(book_name)
-	list_x.appendChild(book_author)
-	list_x.appendChild(book_desc)
-
-	list_x.setAttribute("id", "list_x")
-	book_id.setAttribute("id", "book_id")
-	book_name.setAttribute("id", "book_name")
-	book_author.setAttribute("id", "book_author")
-	book_desc.setAttribute("id", "book_desc")
-
-	document.getElementById("list_of_books").appendChild(list_x)
-	document.getElementById("list_of_books").appendChild(document.createElement("hr"))
+	cell1.innerHTML = "ID";
+	cell2.innerHTML = "NAME";
+	cell3.innerHTML = "AUTHOR";
+	cell4.innerHTML = "DESCRIPTION";
 }
 
-getReq();
+function renderBookList(id, name, author, desc) {
+	var table = document.getElementById("list_of_books");
+
+	var row = table.insertRow(-1);
+
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+
+	cell1.innerHTML = id;
+	cell2.innerHTML = name;
+	cell3.innerHTML = author;
+	cell4.innerHTML = desc;
+}
+
+function refreshBookList() {
+	getReq().then((res) => {
+		all_books = res
+		document.getElementById("list_of_books").innerHTML = "";
+		renderTableHeading();
+		for (i in all_books)
+			renderBookList(all_books[i]["id"], all_books[i]["name"], all_books[i]["author"], all_books[i]["description"]);
+	})
+}
 
 function getReq() {
-	req('GET', '?data=all')
+	return req('GET', '?data=all')
 		.then((res) => {
-			var books = res["books"]
-			for (i in books) {
-				renderBookList(books[i]["id"], books[i]["name"], books[i]["author"], books[i]["description"]);
-			}
+			return res["books"];
 		})
+}
+
+function idExists(id){
+	for (i in all_books)
+		if (all_books[i]["id"] == id)
+			return i
+	return -1
 }
 
 function postReq() {
@@ -71,29 +111,44 @@ function postReq() {
 	req('POST', '', {
 		books: [name, author, desc]
 	}).then((res) => {
-		console.log(res)
+		document.getElementById("post_reply").innerHTML = res["result"]
+		refreshBookList()
 	})
 }
 
 function deleteReq() {
 	var id = document.getElementById("delete_id_i").value;
+	if (idExists(id) == -1) {
+		document.getElementById("delete_reply").innerHTML = " The given id dosen't seem to exist."
+		return
+	}
 
 	req('DELETE', '?id=' + id).then((res) => {
-		console.log(res)
+		document.getElementById("delete_reply").innerHTML = res["result"]
+		refreshBookList()
 	})
 }
 
 function putReq() {
 	var id = document.getElementById("put_id_i").value;
+	var idIndex = idExists(id)
+	if (idIndex == -1) {
+		document.getElementById("put_reply").innerHTML = " The given id dosen't seem to exist."
+		return
+	}
+
 	var name = document.getElementById("put_name_i").value;
 	var author = document.getElementById("put_author_i").value;
 	var desc = document.getElementById("put_description_i").value;
 
-	console.log('fsdfsd')
+	if (name == "") name = all_books[idIndex]["name"]
+	if (author == "") author = all_books[idIndex]["author"]
+	if (desc == "") desc = all_books[idIndex]["description"]
 
 	req('PUT', '', {
 		book: [id, name, author, desc]
 	}).then((res) => {
-		console.log(res)
+		document.getElementById("put_reply").innerHTML = res["result"]
+		refreshBookList()
 	})
 }
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-const os = require('os');
-const fs = require('fs');
 const express = require('express');
 const mysql = require('mysql');
 
 const SERVER_IP = "localhost";
-const SERVER_PORT = 80; // process.env.SERVER_PORT
+const SERVER_PORT = 80;
 
 class Database {
 	constructor() {
@@ -23,7 +21,8 @@ class Database {
 		// check if the database exists, if not, create n initialize it
 		this.runQuery('use bookstore;', (err, data) => {
 			if (err) {
-				var database_init_cmds = ['create database bookstore;',
+				var database_init_cmds = [
+					'create database bookstore;',
 					'use bookstore;',
 					'create table books (id int unique auto_increment, name varchar(64), author varchar(64), description text);'
 				]
@@ -52,24 +51,39 @@ app = express();
 app.use(express.static("public")) // to serve the html, css, js stuff
 
 app.get('/api', (req, res) => {
-	main_database.runQuery('select * from books;', (err, data) => {
-		if (err) console.log("[ MYSQL ] error get book");
+	const urlParams = new URLSearchParams(req._parsedUrl.search);
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-		res.send(JSON.stringify({
-			"books": data
-		}))
-	})
+	if (urlParams.get('data') == "all") {
+		main_database.runQuery('select * from books;', (err, data) => {
+			res.send(JSON.stringify({
+				"books": data
+			}))
+		})
+	} else {
+		var list = urlParams.get('data').split(',')
+		var query = "SELECT * FROM books WHERE "
+		for (var i = 0; i < list.length - 1; i++) {
+			query += "id=" + list[i] + " or "
+		}
+		query += "id=" + list[list.length - 1] + ";"
+
+		main_database.runQuery(query, (err, data) => {
+			res.send(JSON.stringify({
+				"books": data
+			}))
+		})
+	}
 })
 
 app.delete('/api', (req, res) => { // no body for get and delete
 	const urlParams = new URLSearchParams(req._parsedUrl.search);
+	res.setHeader('Access-Control-Allow-Origin', '*');
 
-	main_database.runQuery("DELETE FROM books WHERE id=" + urlParams.get('id'), (err, data) => {
-		if (err) console.log("[ MYSQL ] error deleting book");
-	})
+	main_database.runQuery("DELETE FROM books WHERE id=" + urlParams.get('id'), (err, data) => {})
 
 	res.send(JSON.stringify({
-		"stat": "ok"
+		"result": "ok"
 	}))
 });
 
@@ -77,18 +91,12 @@ app.post('/api', (req, res) => {
 	let body = '';
 	req.on('data', data => body += data)
 	req.on('end', () => {
-		res.writeHead(200, {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'POST, GET',
-		});
+		res.setHeader('Access-Control-Allow-Origin', '*');
 
 		books = JSON.parse(body)["books"];
 
 		var cmd = 'INSERT INTO books (name,author,description) VALUES (\"' + books[0] + '\",\"' + books[1] + '\",\"' + books[2] + '\");'
-		main_database.runQuery(cmd, (err, data) => {
-			if (err) console.log("[ MYSQL ] error post book")
-		})
+		main_database.runQuery(cmd, (err, data) => {})
 
 		res.end(JSON.stringify({
 			"result": "ok"
@@ -100,18 +108,12 @@ app.put('/api', (req, res) => {
 	let body = '';
 	req.on('data', data => body += data)
 	req.on('end', () => {
-		res.writeHead(200, {
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*',
-			'Access-Control-Allow-Methods': 'POST, GET',
-		});
+		res.setHeader('Access-Control-Allow-Origin', '*');
 
 		book = JSON.parse(body)["book"];
 
-		var cmd = 'UPDATE books SET name=\"'+book[1]+'\", author=\"'+book[2]+'\", description=\"'+book[3]+'\" WHERE id='+book[0]+';'
-		main_database.runQuery(cmd, (err, data) => {
-			if (err) console.log("[ MYSQL ] error post book")
-		})
+		var cmd = 'UPDATE books SET name=\"' + book[1] + '\", author=\"' + book[2] + '\", description=\"' + book[3] + '\" WHERE id=' + book[0] + ';'
+		main_database.runQuery(cmd, (err, data) => {})
 
 		res.end(JSON.stringify({
 			"result": "ok"
